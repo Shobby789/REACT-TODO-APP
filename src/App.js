753931aa359928c774c8fa75cd0558ba0337e23d
Shobby789 <smshoaib2001@gmail.com>
2023-03-05 11:05:47 +0500

    created a todo app in react

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import { Button, Input, InputGroup, Container } from "reactstrap";
+import "./App.css";
+import { Trash3Fill } from "react-bootstrap-icons";
+
+function App() {
+  const [taskList, setTaskList] = useState([]);
+  const [task, setTask] = useState("");
+
+  const addTaskHandler = () => {
+    setTaskList([...taskList, task]);
+    setTask("");
+  };
+
+  const handleDelete = (id) => {
+    const newList = taskList.filter((item, index) => {
+      return index !== id;
+    });
+    // console.log(id);
+    setTaskList(newList);
+  };
+
+  return (
+    <>
+      <Container className="text-center">
+        <h3 className="mt-4">Shoaib's Todo App</h3>
+        <Container
+          className="mt-3 w-50 mx-auto border border-warning p-5"
+          style={{ minHeight: "80vh" }}
+        >
+          <InputGroup>
+            <Input
+              className="py-2"
+              value={task}
+              onChange={(e) => setTask(e.target.value)}
+              placeholder="Add tasks..."
+            />
+            <Button color="primary" className="" onClick={addTaskHandler}>
+              Add Task
+            </Button>
+          </InputGroup>
+          <Container className="mt-5">
+            {taskList.map((item, id) => {
+              return (
+                <Container
+                  key={id}
+                  className="border border-warning w-100 mx-auto mt-4 py-2 px-3 d-flex justify-content-between align-items-center rounded"
+                >
+                  <h3>{item}</h3>
+                  <Trash3Fill
+                    size={20}
+                    onClick={() => handleDelete(id)}
+                    role="button"
+                  />
+                </Container>
+              );
+            })}
+          </Container>
+        </Container>
+      </Container>
+    </>
+  );
+}
+
+export default App;
